6096ae95a711fd4e7d4ce525426e7b5e5018e683
Bhaskara Marthi <bhaskara@gmail.com>
2011-10-12 15:54:59 -0700

    add an export

--- package.lisp
+++ package.lisp
@@ -1,7 +1,8 @@
 (defpackage :agent-env
   (:use :cl :cl-utils) 
   (:export :act :reset :reset-to-state :get-state :set-state :at-terminal-state :legal-action-list
-	   :modeled-env :model :sample-transition :sample-initial :is-terminal-state :sample-initial-observation
+	   :modeled-env :model :sample-transition :sample-initial :is-terminal-state
+           :sample-initial-observation
 	   :is-legal-action :legal-action-list-at-state
 
 	   :observe :observe-initial :select-action :reset-local-state :agent-finished
@@ -9,4 +10,5 @@
 
 	   :env-agent-trajectory :execute-agent-in-env :transition :transition-state :transition-reward :transition-observation :transition-action 
 
-	   :stdout-listener :reward-listener :agent-listener :progress-printer))
+	   :stdout-listener :reward-listener :agent-listener :progress-printer
+           ))
